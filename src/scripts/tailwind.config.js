00ae577917e85dcdafbdc0fc46e0e33cf2854aff
Λ B H I N Λ V <ixd.abhi@gmail.com>
2025-03-18 08:14:06 +0530

    tailwind config with brand colors
    Added a separate file with tailwind config and updated it with the brand colors.

---
+++ src/scripts/tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'work-sans': ['Work Sans', 'sans-serif'],
+            }, 
+            colors: { 
+                'deep-purple':'#16006D',
+                'indigo':'#3843D0',
+                'cornflower-blue':'#5D70D2',
+                'sky':'#8EA1FF',
+                'sunset':'#E44434',
+                'mango':'#FC5F36',
+                'marigold':'#FEAF31',
+                'black-grey':'#0F172A',
+                'blue-light':'#94A3B8',
+                'sunset-light':'#FC5F3633',
+                'border-light':' #E2E8F0',
+            },
+        },
+    },
+}
